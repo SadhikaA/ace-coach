0280c6d534ff3174576bc68423a7100cfb599e28
Sadhika Akula <sadhika.akula@berkeley.edu>
2023-04-15 22:12:02 -0700

    button

--- ace-coach.swiftpm/ContentView.swift
+++ ace-coach.swiftpm/ContentView.swift
@@ -1,10 +1,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {        
+    var body: some View {
+        
         VStack {
-            Text("ace").font(.largeTitle).padding()
-            Text("Your personal tennis coach").padding()
+            Text("ace").font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/).fontWeight(.bold).foregroundColor(Color.black).multilineTextAlignment(.center).padding()
+            Text("your personal tennis coach").fontWeight(.medium).multilineTextAlignment(.center).padding()
+            Text("Hello World")
+                .fontWeight(.bold)
+                .font(.title)
+                .padding()
+                .background(Color.purple)
+                .foregroundColor(.white)
+                .padding(10)
+                .border(Color.purple, width: 5)
         }
     }
 }
