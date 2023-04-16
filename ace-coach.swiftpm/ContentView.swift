f9a0e06aa59a56eb8f944042f818a8d2d67ff07f
Sadhika Akula <sadhika.akula@berkeley.edu>
2023-04-16 00:00:13 -0700

    all changes so far

--- ace-coach.swiftpm/ContentView.swift
+++ ace-coach.swiftpm/ContentView.swift
@@ -1,19 +1,44 @@
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        
+        NavigationView {
+            VStack(alignment: .center) {
+                Image("tennis_ball")
+                    .resizable()
+                    .padding(.leading)
+                    .frame(width: 150, height: 100)
+                    .scaledToFit()
+                Text("ace").font(.system(size: 60)).fontWeight(.bold).multilineTextAlignment(.center)
+                Text("your personal tennis coach").fontWeight(.medium).multilineTextAlignment(.center).padding(.bottom)
+                Button("let's ") {
+                    Text("let's play")
+                        .font(.body)
+                        .fontWeight(.medium)
+                        .padding([.top, .bottom], 8.0)
+                        .padding([.leading, .trailing], 25.0)
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                        .cornerRadius(40)
+                }
+            }
+            .padding(.bottom)
+            .preferredColorScheme(.light)
+            
+            VStack(spacing:30) {
+                NavigationLink(destination: RacquetView()) {
+                    Text("Show Racquet View")
+                }
+            }.navigationTitle("Menu")
+        }
+    }
+}
+
+struct RacquetView : View {
+    var body: some View {
         VStack {
-            Text("ace").font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/).fontWeight(.bold).foregroundColor(Color.black).multilineTextAlignment(.center).padding()
-            Text("your personal tennis coach").fontWeight(.medium).multilineTextAlignment(.center).padding()
-            Text("Hello World")
-                .fontWeight(.bold)
-                .font(.title)
-                .padding()
-                .background(Color.purple)
-                .foregroundColor(.white)
-                .padding(10)
-                .border(Color.purple, width: 5)
+            Text("Racquet Selection")
         }
     }
 }
