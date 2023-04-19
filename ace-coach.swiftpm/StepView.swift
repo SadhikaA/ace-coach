0213842e8175a3d73f6206de9eeda2b2354b6a03
Sadhika Akula <sadhika.akula@berkeley.edu>
2023-04-18 23:03:05 -0700

    more changes to step view

--- ace-coach.swiftpm/StepView.swift
+++ ace-coach.swiftpm/StepView.swift
@@ -5,12 +5,40 @@
 import SwiftUI
 
 struct StepView: View {
-    
-    @State var next: String? = nil
+    @State private var next: String? = nil
+    @State private var unlock = false 
     
     var body: some View {
         NavigationView {
-            
+            List {
+                Button(action: {
+                       unlock.toggle()
+                   }) {
+                   if unlock {
+                       Image(systemName: "lock.open.fill").foregroundColor(Color("Green"))
+                   } else {
+                       Image(systemName: "lock.fill").foregroundColor(Color("Green"))
+                   }
+                }
+                Button("Racquet") {
+                    next = "Racquet"
+                }.foregroundColor(Color("Green"))
+                Button("Court") {
+                    next = "Racquet"
+                }.foregroundColor(Color("Green"))
+                Button("Forehand") {
+                    next = "Racquet"
+                }.foregroundColor(Color("Green"))
+                Button("Backhand") {
+                    next = "Racquet"
+                }.foregroundColor(Color("Green"))
+                Button("Serve") {
+                    next = "Racquet"
+                }.foregroundColor(Color("Green"))
+                Button("Congrats") {
+                    next = "Racquet"
+                }.foregroundColor(Color("Green"))
+            }
         }
     }
 }
